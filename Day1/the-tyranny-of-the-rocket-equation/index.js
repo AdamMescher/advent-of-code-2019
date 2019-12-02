0ac87f88d0084f120fadcb23e6e0fa1ff8b5ac26
Adam Mescher <admescher@gmail.com>
2019-12-01 18:28:20 -0700

    Arrive at solutions for both parts of day 1

--- Day1/the-tyranny-of-the-rocket-equation/index.js
+++ Day1/the-tyranny-of-the-rocket-equation/index.js
@@ -1,4 +1,39 @@
+const readline = require('readline');
+const fs = require('fs');
+
 const calculateFuelConsumption = mass => parseInt(mass / 3) - 2;
+const calculateFuelConsumptionWithAddedFuel = mass => {
+    let total = 0;
+    while (mass > 8) {
+      total += calculateFuelConsumption(mass);
+      mass = calculateFuelConsumption(mass);
+    }
+    return total;
+}
 
+const calculateModuleFuelRequirement = async () => {
+    const rl = readline.createInterface({ input: fs.createReadStream('./puzzle-input.txt') });
+    let total = 0;
+    rl.on('line', function(line) {
+        total += calculateFuelConsumption(line);
+        console.log(calculateFuelConsumption(line))
+        console.log(total)
+    });
+    await total;
+    return total;
+}
 
-module.exports = calculateFuelConsumption;
+const calculateModuleFuelRequirementWithAddedFuel = async () => {
+    const rl = readline.createInterface({ input: fs.createReadStream('./puzzle-input.txt') });
+    let total = 0;
+    rl.on('line', function(line) {
+        total += calculateFuelConsumptionWithAddedFuel(line);
+        console.log('TOTAL: ', total)
+    });
+    await total;
+    return total;
+}
+
+calculateModuleFuelRequirementWithAddedFuel();
+
+module.exports = { calculateFuelConsumption, calculateFuelConsumptionWithAddedFuel };
--- Day1/the-tyranny-of-the-rocket-equation/index.test.js
+++ Day1/the-tyranny-of-the-rocket-equation/index.test.js
@@ -4,35 +4,58 @@ For a mass of 1969, the fuel required is 654.
 For a mass of 100756, the fuel required is 33583.
 */
 
-const calculateFuelConsumption = require('./index');
+const {calculateFuelConsumption, calculateFuelConsumptionWithAddedFuel} = require('./index');
 
 describe('Calculate Mass', () => {
     it('Should be able to calculate a value without rounding', () => {
-        const mass = 12;
-        const fuelRequirement = calculateFuelConsumption(mass);
-        const expectedFuel = 2;
-        expect(expectedFuel).toEqual(fuelRequirement);
+        const moduleMass = 12;
+        const expectedModuleFuelRequirement = 2;
+        const actualModuleFuelRequirement = calculateFuelConsumption(moduleMass);
+        expect(expectedModuleFuelRequirement).toEqual(actualModuleFuelRequirement);
     });
     it('Should be able to calculate a value with rounding', () => {
-        const mass = 14;
-        const fuelRequirement = calculateFuelConsumption(mass);
-        const expectedFuel = 2;
-        expect(expectedFuel).toEqual(fuelRequirement);
+        const moduleMass = 14;
+        const expectedModuleFuelRequirement = 2;
+        const actualModuleFuelRequirement = calculateFuelConsumption(moduleMass);
+        expect(expectedModuleFuelRequirement).toEqual(actualModuleFuelRequirement);
     });
     it('Should be able to calculate a value mass with 4 digits', () => {
-        const mass = 1969;
-        const fuelRequirement = calculateFuelConsumption(mass);
-        const expectedFuel = 654;
-        expect(expectedFuel).toEqual(fuelRequirement);
+        const moduleMass = 1969;
+        const expectedModuleFuelRequirement = 654;
+        const actualModuleFuelRequirement = calculateFuelConsumption(moduleMass);
+        expect(expectedModuleFuelRequirement).toEqual(actualModuleFuelRequirement);
     });
     it('Should be able to calculate a value mass with 6 digits', () => {
-        const mass = 100756;
-        const fuelRequirement = calculateFuelConsumption(mass);
-        const expectedFuel = 33583;
-        expect(expectedFuel).toEqual(fuelRequirement);
+        const moduleMass = 100756;
+        const expectedModuleFuelRequirement = 33583;
+        const actualModuleFuelRequirement = calculateFuelConsumption(moduleMass);
+        expect(expectedModuleFuelRequirement).toEqual(actualModuleFuelRequirement);
     });
 });
 
 describe('Read from input file', () => {
-    it('Should be able to read the')
+    it.skip('Should be able to read the mass of the module on the first line of the file and return the required fuel consumption', () => {});
+    it.skip('Should be able to calculate the fuel consumption of two modules on the first and second line of the file and return the sum', () => {});
+    it.skip('Should be able to calculate the fuel consumption of all modules and return the sum', () => {});
+});
+
+describe('Fuel for the additional fuel', () => {
+    it('Should be able to calculate the additional fuel consumption that does not need more than one calculation', () => {
+        const moduleMass = 14;
+        const expectedModuleFuelConsumption = 2;
+        const actualModuleFuelConsumption = calculateFuelConsumptionWithAddedFuel(moduleMass);
+        expect(expectedModuleFuelConsumption).toEqual(actualModuleFuelConsumption);
+    });
+    it('Should be able to calculate the additional fuel consumption that that requires four steps', () => {
+        const moduleMass = 1969;
+        const expectedModuleFuelConsumption = 966;
+        const actualModuleFuelConsumption = calculateFuelConsumptionWithAddedFuel(moduleMass);
+        expect(expectedModuleFuelConsumption).toEqual(actualModuleFuelConsumption);
+    });
+    it('Should be able to calculate the additional fuel consumption that that requires eight steps', () => {
+        const moduleMass = 100756;
+        const expectedModuleFuelConsumption = 50346;
+        const actualModuleFuelConsumption = calculateFuelConsumptionWithAddedFuel(moduleMass);
+        expect(expectedModuleFuelConsumption).toEqual(actualModuleFuelConsumption);
+    });
 })
