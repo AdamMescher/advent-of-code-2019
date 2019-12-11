eb950517a1e935806ec5ce651ec6488e4586cf7f
Adam Mescher <admescher@gmail.com>
2019-12-10 22:34:32 -0700

    Add support for opcode 3 and opcode 4

--- Day05/index.js
+++ Day05/index.js
@@ -1,19 +1,26 @@
-const readline = require('readline');
+const readlineSync = require('readline-sync');
 
 class Intcode {
-    constructor(program) {
+    constructor(program, mode) {
         this.program = program.split(',').map(num => parseInt(num));
         this.current = 0;
     }
 
     run() {
-        while (this.program[this.current] !== 99) {
-            this.step(this.current);
+      const execute = async position => {
+        if (this.program[position] === 99) {
+          console.log(`SOLUTION: `, this.program[0]);
+          return this.program[0];
+        } else {
+          this.positionMode(this.current);
+          execute(this.current);
         }
-        return this.program[0];
+      }
+      execute(this.current);
     }
 
-    step(position) {
+    positionMode(position) {
+        if(this.program[position])
         if (this.program[position] === 1) {
             this.program[this.program[this.current + 3]] = this.program[this.program[this.current + 1]] + this.program[this.program[this.current + 2]];
             this.current += 4;
@@ -21,22 +28,26 @@ class Intcode {
             this.program[this.program[this.current + 3]] = this.program[this.program[this.current + 1]] * this.program[this.program[this.current + 2]];
             this.current += 4;
         } else if (this.program[position] === 3) {
-            const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-            rl.question(`What's your name?`, async (name) => {
-                const userName = await name;
-                console.log(userName);
-                rl.close();
-              })
-            rl.close()
-            this.current += 2;
-
+          const input = readlineSync.question('INPUT: ');
+          this.program[this.program[this.current + 1]] = parseInt(input);
+          this.current += 2;
         } else if (this.program[position] === 4) {
-            console.log('OUTPUT: ', this.program[this.current + 3]);
+            console.log('OUTPUT: ', this.program[this.program[this.current + 1]]);
             this.current += 2;
-        } else { console.error(`Value not recognized at position ${position} - ${program[position]}`) }
+        } else {
+            console.error(this.invalidOpcode(this.program, position));
+        }
+    }
+
+    invalidMode(mode) {
+        return `Unrecognized mode identified: ${mode}. Mode should be either POSITION MODE (0) or PARAMETER MODE (1).`;
+    }
+
+    invalidOpcode(program, position) {
+        return `Opcode not recognized at position ${position} - ${program[position]}.`;
     }
 }
 
-const program = '3,0,4,0,99';
-const computer = new Intcode(program);
+const program = '1002,4,3,4,33';
+const computer = new Intcode(program, 0);
 computer.run();
