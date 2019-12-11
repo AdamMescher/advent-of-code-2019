00d8e469b75d3c2579a87bb4ff534a1bec5ad246
Adam Mescher <admescher@gmail.com>
2019-12-11 11:55:22 -0700

    Refactor Day05 index

--- Day05/index.js
+++ Day05/index.js
@@ -12,39 +12,76 @@ class Intcode {
           console.log(`SOLUTION: `, this.program[0]);
           return this.program[0];
         } else {
-          this.positionMode(this.current);
+          this.step(this.current);
           execute(this.current);
         }
       }
       execute(this.current);
     }
 
-    positionMode(position) {
-        if(this.program[position])
-        if (this.program[position] === 1) {
-            this.program[this.program[this.current + 3]] = this.program[this.program[this.current + 1]] + this.program[this.program[this.current + 2]];
-            this.current += 4;
-        } else if (this.program[position] === 2) {
-            this.program[this.program[this.current + 3]] = this.program[this.program[this.current + 1]] * this.program[this.program[this.current + 2]];
-            this.current += 4;
-        } else if (this.program[position] === 3) {
-          const input = readlineSync.question('INPUT: ');
-          this.program[this.program[this.current + 1]] = parseInt(input);
-          this.current += 2;
-        } else if (this.program[position] === 4) {
-            console.log('OUTPUT: ', this.program[this.program[this.current + 1]]);
-            this.current += 2;
+    advancePostion = distance => this.current += distance;
+
+    generateErrorMessage = (type, value) => {
+        if(type === 'opcode') {
+            return `Opcode not recognized at position ${value} - ${this.program[value]}.`;
         } else {
-            console.error(this.invalidOpcode(this.program, position));
+            return `Unknown error -- ${value}`;
         }
     }
 
-    invalidMode(mode) {
-        return `Unrecognized mode identified: ${mode}. Mode should be either POSITION MODE (0) or PARAMETER MODE (1).`;
+    updatePositionValue(position, value) {
+        this.program[position] = value;
     }
 
-    invalidOpcode(program, position) {
-        return `Opcode not recognized at position ${position} - ${program[position]}.`;
+    add(current) {
+        const sum = this.program[this.program[current + 1]] + this.program[this.program[current + 2]];
+        const position = this.program[this.program[current + 3]];
+        this.updateValueAtPosition(position, sum);
+        this.advancePostion(4);
+    }
+
+    multiply(current) {
+        const product = this.program[this.program[current + 1]] * this.program[this.program[current + 2]];
+        const position = this.program[this.program[current + 3]];
+        this.updateValueAtPosition(position, product);
+        this.advancePostion(4);
+    }
+
+    takeInput(current) {
+        const input = readlineSync.question('INPUT: ');
+        this.program[this.program[current + 1]] = parseInt(input);
+        this.advancePostion(2);
+    }
+
+    displayOutput(current) {
+        console.log('OUTPUT: ', this.program[this.program[current + 1]]);
+        this.advancePostion(2);
+    }
+
+    immediateMode(current) {
+
+    }
+
+    positionMode(current) {
+        if (this.program[current] === 1) {
+            this.add(current);
+        } else if (this.program[current] === 2) {
+            this.multiply(current);
+        } else if (this.program[current] === 3) {
+            this.takeInput(current);
+        } else if (this.program[current] === 4) {
+            this.displayOutput(current);
+        } else {
+            console.error(this.generateErrorMessage('opcode', current));
+        }
+    }
+
+    step(position) {
+        if(this.program[position].toString().length > 2) {
+            this.immediateMode(position);
+        } else {
+            this.positionMode(position);
+        }
     }
 }
 
